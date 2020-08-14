1d254a0067d23dc50ba73594864ab36d41f402c5
ndev <napodevesa@gmail.com>
2020-08-14 13:41:19 +0200

    changes js 14-08

--- clase1.js
+++ clase1.js
@@ -15,22 +15,29 @@ console.log( "Hola " + nombreMay + " "
 console.log( cantidad);
 console.log (str);
 
+return {
+		...persona
+		edad: persona.edad + 1;
+	}
+
 */
 
 
 var resultado = document.getElementById("resultado");
 
-
 var getData = function ()
 { 
-var name = document.getElementById("name").value;
-var nombre = document.getElementById("nombre").value;
-var edad = document.getElementById("edad").value;
-var job = document.getElementById("job").value;
+	var name = document.getElementById("name").value;
+	var nombre = document.getElementById("nombre").value;
+	var edad = document.getElementById("edad").value;
+	var job = document.getElementById("job").value;
+	var peso = document.getElementById("peso").value;
 
-	resultado.innerHTML = imprimirStr(name) + 
-	imprimirEdad(nombre,edad) + imprimirCumpleanios (edad) 
-	+ compararNombre (name, nombre) + compararJob (job);
+		resultado.innerHTML = imprimirStr(name) + 
+		imprimirEdad(nombre,edad) + imprimirCumpleanios (edad) 
+		+ compararNombre (name, nombre) + compararJob (job) 
+		+ imprimirSiEsMayorDeEdad(edad) + 
+		permitirAcceso (edad) + engordar (peso) + engordarEdad (edad,peso);
 
 }
 
@@ -39,38 +46,26 @@ function imprimirEdad (n, e)
 	var nombre = n;
 	var edad = e;
 	document.write (nombre + " tiene " + edad + " years ");
-
 }
 
 function imprimirStr (n)
 {
 	var str = n.substr(0,3);
 	document.write (str.toUpperCase() + "<br/>");
-
 }
 
 function imprimirCumpleanios (e)
-
 {
 	var edad = parseInt (e) + 1;
 	document.write ("cumplira: " + edad + "<br/>");
-
-	/*
-	return {
-		...persona
-		edad: persona.edad + 1;
-	}
-	*/
-
 }
 
 function compararNombre (n, d)
 {
 	if (n===d)
 	{
-		document.write ("Error son la misma persona");
+		document.write ("Error son la misma persona <br/> ");
 	}
-
 }
 
 function compararJob (j)
@@ -80,11 +75,70 @@ function compararJob (j)
 
 	if (j === ing || j === med)
 	{
-		document.write ("tu profesion sirve");
-	} else
+		document.write ("Tu profesion sirve <br/> ");
+	} 
+	else
 	{
-		document.write ("tu profesion NO sirve");
+		document.write ("Tu profesion NO sirve <br/>");
+	}
+}
+
+const MAYOR_EDAD = 18;
+
+function esMayorDeEdad(s) 
+{
+	if (s >= MAYOR_EDAD)
+	{
+		return true;
+	}
+}
+
+function imprimirSiEsMayorDeEdad(s) 
+{
+	if (esMayorDeEdad(s))
+	{
+		document.write (" Es mayor de edad <br/>");
+	} else {
+		document.write (" NO es mayor de edad <br/>");
+	}
+}
+
+function permitirAcceso (s)
+{
+	if (!esMayorDeEdad(s))
+	{
+		document.write (" acceso DENEGADO <br/>");
+	}
+	else
+	{
+		document.write (" acceso PERMITIDO <br/>");
+	}
+}
+
+const dias = 365;
+
+function engordar (s)
+{
+	var pesoFinal= parseInt (s)
+
+	for (var i=0; i <= dias; i++)
+	{
+		pesoFinal = pesoFinal +  1;
 	}
 
+document.write (" Peso a fin de anio:  " + pesoFinal + "<br/>");
+}
+
+
+function engordarEdad (edad,s)
+{
+	var pesoF= parseInt(s)
+
+	while (edad <= dias)
+	{
+		pesoF = pesoF +  2;
+		edad++
+	}
+document.write (" Aumento de dos kilos por year:  " + pesoF);
 }
 
