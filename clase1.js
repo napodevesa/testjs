de5b84d3d64ef59c143a8ef282225cb91af0ff1e
ndev <napodevesa@gmail.com>
2020-08-16 01:16:25 +0200

    filter js

--- clase1.js
+++ clase1.js
@@ -21,7 +21,6 @@ return {
 	}
 
 
-*/
 
 var resultado = document.getElementById("resultado");
 
@@ -218,5 +217,72 @@ switch (signo)
 }
 
 
+
+Arrays: 
+.length = cuantos elementos tiene el array.
+
+var personas = [sacha, juan, emilio, jorge, luis]
+
+for (var i = 0 ; i < personas.length ; i++) {
+	
+	var personas = personas[i]
+
+	console.log (persona.nombre + "mide" + persona.altura + "metros");
+}
+console.log ("${persona.nombre} mide ${persona.altuta} metros")
+*/
+
+
+var sacha = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    altura: 1.72
+}
+
+var alan = {
+    nombre: 'Alan',
+    apellido: 'Perez',
+    altura: 1.86
+}
+
+var martin = {
+    nombre: 'Martin',
+    apellido: 'Gomez',
+    altura: 1.85
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Juarez',
+    altura: 1.71
+}
+
+var vicky = {
+    nombre: 'Vicky',
+    apellido: 'Zapata',
+    altura: 1.56
+}
+
+var paula = {
+    nombre: 'Paula',
+    apellido: 'Barros',
+    altura: 1.76
+}
+
+//array
+var personas = [sacha, alan, martin, dario, vicky, paula]
+
+const esAlta = ({altura}) => altura >= 1.8
+const esBaja = ({altura}) => altura < 1.8 //RETO
+
+
+
+//metodo para filtrar arreglos (recibe un array como parametro)
+var personasAltas = personas.filter(esAlta)
+var personasBajas = personas.filter(esBaja)
+
+
+console.log(personasAltas)
+console.log(personasBajas)
 
 
